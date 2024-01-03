3b8c5d973486f0e1e047bdcc72d2329770eabaca
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-03 03:17:58 +0000

    Update

--- src/main/java/com/nate/spring/security/Spring/Security/Demo/registration/RegistrationController.java
+++ src/main/java/com/nate/spring/security/Spring/Security/Demo/registration/RegistrationController.java
@@ -3,10 +3,7 @@ package com.nate.spring.security.Spring.Security.Demo.registration;
 
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "api/v1/registration")
@@ -15,6 +12,7 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
 
         return registrationService.register(request);
