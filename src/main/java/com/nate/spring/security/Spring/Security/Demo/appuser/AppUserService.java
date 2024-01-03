1dc8b4687ce5def8e36ce24826a6d1a7059226e8
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-03 03:40:33 +0000

    Update

--- src/main/java/com/nate/spring/security/Spring/Security/Demo/appuser/AppUserService.java
+++ src/main/java/com/nate/spring/security/Spring/Security/Demo/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG =
             "User with email %s not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     @Override
@@ -22,4 +24,20 @@ public class AppUserService implements UserDetailsService {
                 .orElseThrow(() ->
                         new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
+
+    public String signUpUser(AppUser appUser){
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail())
+                .isPresent();
+        if (userExists){
+            throw new IllegalStateException("Email already taken");
+        }
+        String encodedPassword =
+                bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+        appUserRepository.save(appUser);
+        return "It Works password is " + appUser.getPassword() +
+                "Encoded Password is " + encodedPassword;
+    }
+
 }
