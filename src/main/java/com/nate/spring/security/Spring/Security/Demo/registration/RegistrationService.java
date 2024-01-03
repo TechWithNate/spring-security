ca6185cd8b9a1717d8ed7f85f72f183ed0277fc5
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-03 03:40:52 +0000

    Update

--- src/main/java/com/nate/spring/security/Spring/Security/Demo/registration/RegistrationService.java
+++ src/main/java/com/nate/spring/security/Spring/Security/Demo/registration/RegistrationService.java
@@ -1,12 +1,29 @@
 package com.nate.spring.security.Spring.Security.Demo.registration;
 
+import com.nate.spring.security.Spring.Security.Demo.appuser.AppUser;
+import com.nate.spring.security.Spring.Security.Demo.appuser.AppUserRole;
+import com.nate.spring.security.Spring.Security.Demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
 
     public String register(RegistrationRequest request) {
-        return "This is working so far";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("Invalid Email");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstname(),
+                request.getLastname(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
