7da5eeaccea031d96dd1aae105ce5e2995585041
Dzreke-Poku Nathan <dzrekenathan2002@gmail.com>
2024-01-16 23:55:01 +0000

    Update

--- src/main/java/com/nate/spring/security/Spring/Security/Demo/registration/RegistrationService.java
+++ src/main/java/com/nate/spring/security/Spring/Security/Demo/registration/RegistrationService.java
@@ -39,7 +39,7 @@ public class RegistrationService {
         String link1 = "https://nate-spring-security-c47ddf4f05f0.herokuapp.com/api/v1/registration/confirm?token=" + token;
         emailSender.send(
                 request.getEmail(),
-                buildEmail(request.getFirstname(), link1));
+                buildEmail(request.getFirstname(), link));
         return token;
     }
 
